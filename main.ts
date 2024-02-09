e785ca3cc7b91b9c0efe09f13b53f3d1eb61bd28
Thomas Whittle <tewhittle@gmail.com>
2024-02-09 12:28:24 -0500

    Add icon to ribbon

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ import { Notice, Plugin } from "obsidian";
 
 export default class MyPlugin extends Plugin {
     async onload() {
-        new Notice("Hello");
+        this.addRibbonIcon("eye", "Peek Into the Dark", () => {
+            new Notice("Boo!");
+        })
     }
 
     onunload() {
